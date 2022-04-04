31cd8dc634640944f4fb06b1b1705e0d330b4c09
collins <collinsboniface1000@gmail.com>
2022-04-04 10:21:35 +0300

    Having a lock in Strings and integers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use std::fmt::format;
+
 // Created a new function
-// Passing a 64 bit signed integer type in the function
+// Passing a 64 bit signed integer type in the function (signed and unsigned)
 // Using a formated string to print the number in the parameter
-fn greet(x: String) {
-    println!("Hello to {}", x);
+fn greet(x: &str) -> String {
+    format!("Hello to {}", x)
 }
 
 fn main() {
     // Change the String into a Capital String
     // String is located at the Heap
-    let my_greeting = "Hello, world".to_string();
-
-    greet(my_greeting);
+    // &str Going to the actuall address
+    let name: &str = "Rust";
+    let result = greet(name);
+    println!("My greeting: {}", result);
 }
