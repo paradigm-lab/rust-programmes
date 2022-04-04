52f75b681bedef4b947e44f31cbd3884e506291c
collins <collinsboniface1000@gmail.com>
2022-04-04 10:06:07 +0300

    Assigned a String into a variable and passed it into a function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Created a new function
+// Passing a 64 bit signed integer type in the function
+// Using a formated string to print the number in the parameter
+fn greet(x: String) {
+    println!("Hello to {}", x);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Change the String into a Capital String
+    // String is located at the Heap
+    let my_greeting = "Hello, world".to_string();
+
+    greet(my_greeting);
 }
