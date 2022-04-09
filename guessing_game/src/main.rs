0a3b41e6dfe236bfa4c4f365b4081dac99472e84
collins <collinsboniface1000@gmail.com>
2022-04-09 10:59:06 +0300

    Updated the toml file to use the rand crate and the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,30 @@
+// Input output(IO) Library
+// The IO library cames with the standard library (which is known as std)
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input the guess.");
+
+    // Defining a mutable variable that means the variable can change
+    // A String type is provided by the standard library that is growable
+    // Growable means that we don't need to specify the size of the String
+    // ::new line indicates that new is an associated function of the String type.
+    // To summarize the line has created mutable variable that is currently bound to a new, empty instance of a String.
+    let mut guess = String::new();
+
+    // stdin can return a type and the type is io result
+    // IO result can either be Ok or Err
+    // When Err means the operation failed and contains information about how or why the operation failed.
+    // and .expect while handle the error
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    
+    // String we saved the user's input in. The set of curly brackets {} is a placeholder
+    
+    println!("You guessed: {}", guess);
 }
