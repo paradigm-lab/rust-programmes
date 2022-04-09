5a2988bdf3feb4c5ba8e678b169aac31fcedcb17
collins <collinsboniface1000@gmail.com>
2022-04-09 10:57:53 +0300

    Update the gitignore file

--- guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.d
+++
@@ -1,5 +0,0 @@
-/home/collin/paradigm/rust_programs/guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.rmeta: src/main.rs
-
-/home/collin/paradigm/rust_programs/guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.d
+++
@@ -1,5 +0,0 @@
-/home/collin/paradigm/rust_programs/guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.rmeta: src/main.rs
-
-/home/collin/paradigm/rust_programs/guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.d: src/main.rs
-
-src/main.rs:
