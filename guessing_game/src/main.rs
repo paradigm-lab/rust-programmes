c4d43b03c0325680a01619dad3a5faf62a4c3283
collins <collinsboniface1000@gmail.com>
2022-04-09 13:06:13 +0300

    Added all of the stuffs in a infinity loop so we need to handle that by exit when we win the game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,37 +11,39 @@ fn main() {
 
     println!("The Secret number is: {}", secret_number);
 
-    println!("Please input the guess.");
+    loop {
+        println!("Please input the guess.");
 
-    // Defining a mutable variable that means the variable can change
-    // A String type is provided by the standard library that is growable
-    // Growable means that we don't need to specify the size of the String
-    // ::new line indicates that new is an associated function of the String type.
-    // To summarize the line has created mutable variable that is currently bound to a new, empty instance of a String.
-    let mut guess = String::new();
+        // Defining a mutable variable that means the variable can change
+        // A String type is provided by the standard library that is growable
+        // Growable means that we don't need to specify the size of the String
+        // ::new line indicates that new is an associated function of the String type.
+        // To summarize the line has created mutable variable that is currently bound to a new, empty instance of a String.
+        let mut guess = String::new();
 
-    // stdin can return a type and the type is io result
-    // IO result can either be Ok or Err
-    // When Err means the operation failed and contains information about how or why the operation failed.
-    // and .expect while handle the error
+        // stdin can return a type and the type is io result
+        // IO result can either be Ok or Err
+        // When Err means the operation failed and contains information about how or why the operation failed.
+        // and .expect while handle the error
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // Rust allows us to shadow the previous value of guess (Shadowing)
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Rust allows us to shadow the previous value of guess (Shadowing)
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    // String we saved the user's input in. The set of curly brackets {} is a placeholder
-    println!("You guessed: {}", guess);
+        // String we saved the user's input in. The set of curly brackets {} is a placeholder
+        println!("You guessed: {}", guess);
 
-    // Here it's comparing the guess to the secret_number.
-    // Then it returns a variant of the ordering enum we brought into scope
-    // We use a match expression to decide what to do next based on which variant of Ordering,
-    // was returned from the call to cmp with the value in guess and secret_number
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // Here it's comparing the guess to the secret_number.
+        // Then it returns a variant of the ordering enum we brought into scope
+        // We use a match expression to decide what to do next based on which variant of Ordering,
+        // was returned from the call to cmp with the value in guess and secret_number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
