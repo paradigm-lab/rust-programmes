c934af84da6bae3aefa8b44b3fde23a4507abfeb
collins <collinsboniface1000@gmail.com>
2022-04-11 13:59:05 +0300

    Created a new function and iterator over a list using a for loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -12,6 +12,9 @@ fn main() {
 
     // Conditional Loops with while
     countdown_from(5);
+
+    // Iteration over a collection in an array
+    countdown_to_five();
 }
 
 fn count_to_ten() -> u32 {
@@ -34,4 +37,11 @@ fn countdown_from(x: u32) {
         number -= 1;
     }
     println!("LIFTOFF!!!");
+}
+
+fn countdown_to_five() {
+    let numbers = [1, 2, 3, 4, 5];
+    for number in numbers.iter() {
+        println!("{}", number);
+    }
 }
