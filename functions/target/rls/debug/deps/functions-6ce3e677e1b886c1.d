247fdea7051a2d06819debfff66411a820340bc9
collins <collinsboniface1000@gmail.com>
2022-04-11 11:04:55 +0300

    Created a new project for the functions in rust

---
+++ functions/target/rls/debug/deps/functions-6ce3e677e1b886c1.d
@@ -0,0 +1,5 @@
+/home/collin/paradigm/rust_programs/functions/target/rls/debug/deps/functions-6ce3e677e1b886c1.rmeta: src/main.rs
+
+/home/collin/paradigm/rust_programs/functions/target/rls/debug/deps/functions-6ce3e677e1b886c1.d: src/main.rs
+
+src/main.rs:
