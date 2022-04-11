891a366a35b45e3b2e605ded819120beff3d9600
collins <collinsboniface1000@gmail.com>
2022-04-11 13:38:35 +0300

    Created a f(x) that will return a result after breaking out of the loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -5,4 +5,20 @@ fn main() {
         println!("Again!");
     }
     */
+
+    // Return values from loops
+    let value = count_to_ten();
+    println!("The result is: {}", value);
+}
+
+fn count_to_ten() -> u32 {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    result
 }
