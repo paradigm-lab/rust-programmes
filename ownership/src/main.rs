38d80473008166aba1243dd7c3e296cdfde83458
collins <collinsboniface1000@gmail.com>
2022-04-12 23:58:04 +0300

    Created a simple copy from the stack for the fixed values

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,4 +8,11 @@ fn main() {
                                        // Do stuff with s
     s.push_str(", Rustaceans"); // Appends a literal to a String
     println!("Our String is: {s}");
+
+    /*
+    Integers are simple values and stored
+    in the stack so in this case y is the copy of 5.
+    */
+    let x = 5;
+    let y = x;
 } // This scope is now over, and s is no longer valid
