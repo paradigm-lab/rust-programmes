f982456d36c9b247ac1382e3e69d14fb2c9f0cba
collins <collinsboniface1000@gmail.com>
2022-04-13 02:29:08 +0300

    Created the f(x) that return the move and copy values from the heap and stack

--- ownership/src/ownership_and_functions.rs
+++ ownership/src/ownership_and_functions.rs
@@ -17,6 +17,16 @@ fn main() {
   println!("x is now: {x}");
 
   // println!("s is now: {s}");
+
+  let s1 = gives_ownership();
+  // Moved s2 to s3
+  let s2 = String::from("Hello Rustaceans");
+  let s3 = takes_and_gives_back(s2);
+
+  println!("s1: {s1}, s3: {s3}");
+
+  let (s4, len) = calculate_length(s3);
+  println!("The length of '{}' is: {}", s4, len);
 }
 
 fn take_ownership(some_string: String) {
@@ -25,4 +35,27 @@ fn take_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
   println!("{some_integer}");
+}
+
+// This function will move its return values into the
+// function that calls it
+fn gives_ownership() -> String {
+  // some_string comes into scope
+  let some_string = String::from("hello");
+
+  // some_string is returned and moves out to the
+  // calling function
+  some_string
+}
+
+// Will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {
+  //a_string is returned and moves out to the calling function.
+  a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+  let length = s.len(); // len() returns the length of a String
+
+  (s, length)
 }
