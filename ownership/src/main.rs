a13d36eaf4034a5f30defaade5c47d1b2dcace27
collins <collinsboniface1000@gmail.com>
2022-04-12 22:48:37 +0300

    Created a string literal using the String type which is allocated in the heap and used the associative f(x) from to get the string

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-}
+    // Variable validation
+    // s is not valid here, it's not yet declared
+    let s = String::from("hello"); // s is valid from this point forward
+                                   // Do stuff with s
+    println!("Our String is: {s}")
+} // This scope is now over, and s is no longer valid
