8ee876d3996811d9fac6b7959af5b22642daf7be
collins <collinsboniface1000@gmail.com>
2022-04-12 23:03:37 +0300

    Used the String push_str method to append into the string it's because String is on the heap and the literal string is on the stack

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     // Variable validation
+
+    let _string_literal = "String Literal";
+
     // s is not valid here, it's not yet declared
-    let s = String::from("hello"); // s is valid from this point forward
-                                   // Do stuff with s
-    println!("Our String is: {s}")
+    let mut s = String::from("Hello"); // s is valid from this point forward
+                                       // Do stuff with s
+    s.push_str(", Rustaceans"); // Appends a literal to a String
+    println!("Our String is: {s}");
 } // This scope is now over, and s is no longer valid
