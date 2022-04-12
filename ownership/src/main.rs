fad84651ffc341ad73ab2f27fbb7758f3b85160c
collins <collinsboniface1000@gmail.com>
2022-04-13 00:49:00 +0300

    Implemented the clone method to see the deep copy implementation

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -15,4 +15,25 @@ fn main() {
     */
     let x = 5;
     let y = x;
+    println!("{y}");
+    // Immutable String
+    /*
+        A pointer holds the contents of the String,
+        A length -> How much memory (In bytes)
+        A capacity -> Total amount of memory (In bytes)
+    */
+    // We are not copying hello but
+    // We are copying the data from the stack
+
+    // This means s1 is no longer existing
+    // This is an example of a "move"
+    // let s1 = String::from("Hello");
+    // let s2 = s1;
+
+    // This is a deep copy and it mantain the value
+    // This is an example of a "Clone"
+    let c1 = String::from("Hello");
+    let c2 = c1.clone();
+
+    println!("c1: {c1}, c2: {c2}");
 } // This scope is now over, and s is no longer valid
