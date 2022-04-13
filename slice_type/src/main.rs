add83bc78483772a5f6f03161dffa651aab85bf1
collins <collinsboniface1000@gmail.com>
2022-04-13 10:39:07 +0300

    Created a new project for the slice type which and defined a f(x) which will return the index of the first word in the array

---
+++ slice_type/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let s = String::from("What's up Rustaceans");
+    let word = first_word(&s);
+    println!("The first word in {} is {}", s, word);
+}
+
+// Passing a String reference in the first_word f(x)
+// It's because we don't want the ownership
+fn first_word(s: &String) -> usize {
+    // We'll convert our String to an array of bytes using as_bytes method
+    let bytes = s.as_bytes();
+
+    // Created an iterator over the array of bytes using the iter method
+    // iter method returns each element in a collection and
+    // Enumerate wraps the result of Iter and
+    // Return each element as a tuple instead.
+
+    // The first element of the tuple returned from enumerate is the index,
+    // The second element is a reference to the element
+
+    // Because enumerate() will return a tuple
+    // We then use a pattern to destructure that tuple.
+    for (i, &item) in bytes.iter().enumerate() {
+        // We search for the byte that represents the space by using the byte literal syntax.
+        // if we find a space we return the position.
+        if item == b' ' {
+            return i;
+        }
+    }
+    // Otherwise we return the length of the String
+    s.len()
+}
