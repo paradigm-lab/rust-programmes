ff15c9e3f90fb6c2aeaac0e4b376cb3d5c5e14df
collins <collinsboniface1000@gmail.com>
2022-04-13 09:08:42 +0300

    Created f(x) with the dangling pointer and the one with no dangling pointer or hangling pointers where by the compiler won't compile if we have a dangling pointer

---
+++ ownership/src/dangling_reference.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // let reference_to_nothing = dangle();
+    let no_dangling = no_dangle();
+    println!("No Dangling Pointer: {no_dangling}");
+}
+
+/*
+// Not going to work as 's' falls out of scope and
+// &s point to nothing
+fn dangle() -> &String {
+  let s = String::from("Hello, Rustaceans");
+  &s
+}
+*/
+
+// Works because we actually return the String
+// This works without any problems. Ownershiip is moved out, and nothing is deallocated.
+fn no_dangle() -> String {
+    let s = String::from("Hello");
+    s
+}
+
+/*
+    Rules of References
+    -> Let's recap what we've discussed about references:
+        -> At any given time, you can have either one mutable reference or any number of immutable references.
+        -> References must always be valid.
+*/
