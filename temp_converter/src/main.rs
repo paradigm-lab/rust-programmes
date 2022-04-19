0d1d43646e37508a2a1a175d2d442fa1209b4d71
collins <collinsboniface1000@gmail.com>
2022-04-19 11:10:00 +0300

    Created new f(x)

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+	println!("Please input a temperature in Fahrenheit.");
+
+	let mut fahrenheit = String::new();
+
+        io::stdin()
+            .read_line(&mut fahrenheit)
+            .expect("Failed to read line");
+
+        let fahrenheit: f64 = match fahrenheit.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                panic!("Please input a number.");
+            }
+        };
+
+}
+
+
+fn fahrenheit_to_celcius(f: f64) -> f64 {
+	(f - 32.0) * (5.0 / 9.0)
+}
+
+
+fn celcius_to_fahrenheit(c: f64) -> f64 {
+	(c * 9.0) / 5.0 + 32.0
 }
