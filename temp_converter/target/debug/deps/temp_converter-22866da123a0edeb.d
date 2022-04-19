28007a54253026d6cce875d43db7f5f0bfa133b2
collins <collinsboniface1000@gmail.com>
2022-04-19 11:16:38 +0300

    Removed the binary files from the git cache

--- temp_converter/target/debug/deps/temp_converter-22866da123a0edeb.d
+++
@@ -1,5 +0,0 @@
-/home/collin/paradigm/rust_programs/temp_converter/target/debug/deps/temp_converter-22866da123a0edeb: src/main.rs
-
-/home/collin/paradigm/rust_programs/temp_converter/target/debug/deps/temp_converter-22866da123a0edeb.d: src/main.rs
-
-src/main.rs:
--- temp_converter/target/debug/temp_converter.d
+++
@@ -1,1 +0,0 @@
-/home/collin/paradigm/rust_programs/temp_converter/target/debug/temp_converter: /home/collin/paradigm/rust_programs/temp_converter/src/main.rs
