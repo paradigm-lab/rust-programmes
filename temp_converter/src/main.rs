834f51600295fa1536bd97f44f06c3da923f52db
collins <collinsboniface1000@gmail.com>
2022-04-20 08:26:11 +0300

    Combined two f(x) to make one f(x)

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -52,28 +52,29 @@ fn main() {
 	*/
 }
 
-fn get_fahrenheit_temperature_from_string(temp_string: &str) -> &str {
+fn get_temperature_from_string(temp_string: &str) -> (f64, &str) {
 	let temp_string_bytes = temp_string.as_bytes();
+	let mut temperature: &str = "";
+	let mut unit = "fahrenheit";
 
 	for (i, &item) in temp_string_bytes.iter().enumerate() {
 		// Using the byte literals to check for the character in the bytes
 		if item == b'F' || item == b'f' {
-			return &temp_string[0..i];
+			temperature = &temp_string[0..i];
+			break;
+		} else if item == b'C' || item == b'c' {
+			temperature = &temp_string[0..i];
+			unit = "celcius";
+			break;
 		}
 	}
-	""
-}
-
-fn get_celsius_temperature_from_string(temp_string: &str) -> &str {
-	let temp_string_bytes = temp_string.as_bytes();
-
-	for (i, &item) in temp_string_bytes.iter().enumerate() {
-		// Using the byte literals to check for the character in the bytes
-		if item == b'C' || item == b'c' {
-			return &temp_string[0..i];
+	let temperature: f64 = match temperature.parse() {
+		Ok(num) => num,
+		Err(_) => {
+			panic!("Invalid temperature!")
 		}
-	}
-	""
+	};
+	return (temperature, unit);
 }
 
 fn fahrenheit_to_celcius(f: f64) -> f64 {
