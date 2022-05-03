5e4c597677dbf00eb1e811dd740c80699ed4db89
collins <collinsboniface1000@gmail.com>
2022-05-03 17:00:08 +0300

    Refactoring to using structs in the temperature converter program which makes more clean structure

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
+struct Temperature<'a> {
+    temp: f64,
+    unit: &'a str,
+}
+
+
 fn main() {
 	println!("Please input a temperature...");
 
@@ -9,19 +15,21 @@ fn main() {
 		.read_line(&mut temperature)
 		.expect("Failed to read line");
 
-	let (temperature, unit) = get_temperature_from_string(temperature.trim());
+	// let (temperature, unit) = get_temperature_from_string(temperature.trim());
 
-	if unit == "celsius" {
+	let temperature: Temperature = get_temperature_from_string(temperature.trim());
+	
+    if temperature.unit == "celsius" {
 		println!(
 			"{}C is equal to {}F",
-			temperature,
-			celcius_to_fahrenheit(temperature)
+			temperature.temp,
+			celcius_to_fahrenheit(temperature.temp)
 		);
-	} else if unit == "fahrenheit" {
+	} else if temperature.unit == "fahrenheit" {
 		println!(
 			"{}F is equal to {}C",
-			temperature,
-			fahrenheit_to_celcius(temperature)
+			temperature.temp,
+			fahrenheit_to_celcius(temperature.temp)
 		);
 	}
 
@@ -31,6 +39,36 @@ fn main() {
 	*/
 }
 
+
+fn get_temperature_from_string(temp_string: &str) -> Temperature {
+	let temp_string_bytes = temp_string.as_bytes();
+	let mut temp: &str = "";
+	let mut unit = "fahrenheit";
+
+	for (i, &item) in temp_string_bytes.iter().enumerate() {
+		// Using the byte literals to check for the character in the bytes
+		if item == b'F' || item == b'f' {
+			temp = &temp_string[0..i];
+			break;
+		} else if item == b'C' || item == b'c' {
+			temp = &temp_string[0..i];
+			unit = "celsius";
+			break;
+		}
+	}
+	let temp: f64 = match temp.parse() {
+		Ok(num) => num,
+		Err(_) => {
+			panic!("Invalid temperature!")
+		}
+	};
+	return Temperature {
+        temp, 
+        unit,
+    };
+}
+
+/*
 fn get_temperature_from_string(temp_string: &str) -> (f64, &str) {
 	let temp_string_bytes = temp_string.as_bytes();
 	let mut temperature: &str = "";
@@ -55,6 +93,7 @@ fn get_temperature_from_string(temp_string: &str) -> (f64, &str) {
 	};
 	return (temperature, unit);
 }
+*/
 
 fn fahrenheit_to_celcius(f: f64) -> f64 {
 	(f - 32.0) * (5.0 / 9.0)
