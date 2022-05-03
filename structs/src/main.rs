a71c3d3c09bdda820e79a114b0da77b72b8214e7
collins <collinsboniface1000@gmail.com>
2022-05-03 13:34:58 +0300

    Exploring Structs in rust by defining and instantiating structs and Using the field Init shorthand

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+	struct User {
+		// We define the names and types of the pieces of data, which we call fields.
+		username: String,
+		email: String,
+		sign_in_count: u64,
+		active: bool,	
+	}
+	
+	let mut user1 = User {
+		email: String::from("younggods117@twitch.tv"),
+		username: String::from("youvnggod"),
+		active: true,
+		sign_in_count: 1,	
+	};
+
+	fn build_user(email: String, username: String) -> User {
+		User {	
+			email,
+			username,
+			active: true,
+			sign_in_count: 1,	
+		}
+
+	}
+
+	// We can use dot notation to get a specific value from a struct
+	println!("User's email is {}", user1.email);
+	
+	user1.username = String::from("Paradigm");
+	println!("User's email is {}", user1.username);
+	
+	let user2 = build_user(String::from("test@testington.com"), String::from("testTestingsworth"));
+	println!("User2 email: {}, username: {}", user2.email, user2.username);
 }
+
+
