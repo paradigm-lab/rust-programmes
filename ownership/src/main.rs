3e45c7f16712fa148a04067841bb39c70386d3f2
collin <collin.collindigm.com>
2022-07-23 12:06:29 +0300

    Explore more about ownership in String data structure

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,32 @@
 fn main() {
     // Variable validation
 
-    let _string_literal = "String Literal";
+    // _string_literal refert to a string literal
+    // _string_literal is not valid here, it's not yet declared
+    let _string_literal = "String Literal"; // _string_literal is valid from this point forward (Validity)
+                                            // Do stuff with _string_literals
 
-    // s is not valid here, it's not yet declared
-    let mut s = String::from("Hello"); // s is valid from this point forward
-                                       // Do stuff with s
+    /*
+        Why can String be mutated but literals cannot? The difference is how these two types deal with memory.
+        String literal is stored on the stack while,
+        String type is store on the heap
+        With String type, in order to support a mutable, growable piece of text, we need to allocate an amount of memory on the heap,
+        unknown at compile time, to hole the contents. This means:
+            * The memory must be requested from the memory allocator at runtime.
+            * We need a way of returning this memory to the allocator when we're done with our String
+
+        When it goes out of scope rust call a special f(x) drop automatically at the closing curly bracket.
+
+        In C++, This pattern of deallocating is Resource Acquisition Is Initialization (RAII)
+    */
+
+    let mut s = String::from("Hello"); // :: Operator that allows us to namespace this particular from f(x)
     s.push_str(", Rustaceans"); // Appends a literal to a String
     println!("Our String is: {s}");
 
     /*
-    Integers are simple values and stored
-    in the stack so in this case y is the copy of 5.
+        Integers are simple values and stored
+        in the stack so in this case y is the copy of 5.
     */
     let x = 5;
     let y = x;
