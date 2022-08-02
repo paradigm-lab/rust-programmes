95078d2d346c232ce9c91d733b08b9ad5d46f38c
paradigm-lab <collin@collindigm.com>
2022-08-02 09:07:04 +0300

    Provided notes about Slice and String slices

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -21,9 +21,10 @@ fn main() {
 
 // Passing a String reference in the first_word f(x)
 // It's because we don't want the ownership
-fn first_word(s: &str) -> usize {   // usize is the type used for the size of an array or String or Index
+fn first_word(s: &str) -> usize {
+    // usize is the type used for the size of an array or String or Index
     // We'll convert our String to an array of bytes using as_bytes method
-    let bytes = s.as_bytes();   // Returns a byte slice of this String's content(The inverse of this method is from_utf8)
+    let bytes = s.as_bytes(); // Returns a byte slice of this String's content(The inverse of this method is from_utf8)
 
     // Created an iterator over the array of bytes using the iter method
     // iter method returns each element in a collection and
@@ -95,3 +96,33 @@ fn chunck_second_word(s: &str) -> &str {
         ""
     }
 }
+
+/*
+    Notes:
+        String slices is a reference to part of a String.
+        The slice data structure stores the starting position and the length of the slice,
+            Which corresponds to ending_index minux starting_index
+
+        let s = String::from("hello world");
+        let world = &s[6..11];
+        "world" would be a slice that contains a pointer to the byte at index 6 of s with a length value of 5
+
+        If index starts at 0 we can use [..2] (Short hand notation) likely with the end [5..]
+        The entire string is [..]
+
+        String slice range indices must occur at valid UTF-8 character boundaries.
+
+        Type that signifies "String slice" is written as (&str) String type slice
+        &str is a type that represent chank or a String or the entire string
+
+        String literal are slices.
+        Pointing to specific point of the binary. String literals are immutable; 
+        &str is a immutable reference
+
+        Other Slices
+        String slices, you might imagine, are specific to strings. But there's a more general slice type, too.
+        Just as we might want to refer to a part of a string, We might want to refer to part of an array. 
+        eg: let a = [1, 2, 3, 4, 5];
+            let slice = &a[1..3];
+
+*/
