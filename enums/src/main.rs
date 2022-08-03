41638b45b9b29cd1b6e0faf5d1928c31cc9e9b07
paradigm-lab <collin@collindigm.com>
2022-08-03 12:38:28 +0300

    Fixing up the errors

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,6 +1,7 @@
+#[derive(Debug)]
 enum IpAddr {
-    V4(u8, u8, u8, u8), 
-    v6(String),
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 /*
@@ -10,11 +11,19 @@ struct IpAddr {
 }
 */
 
-fn route(ip_kind: IpAddrKind) {}
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn _route(_ip_kind: IpAddrKind) {}
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
+
+    println!("The IpAddr Version Kind is {:#?} and {:#?}", four, six);
 
     /*
     let home = IpAddr {
@@ -28,6 +37,11 @@ fn main() {
     }
     */
 
-    let home = IpAddr::V4(String::from("127.0.0.1"))
-    let loopback = IpAddr::V6(String::from("::1"))
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!(
+        "Home Ip is {:#?} \n and the Ip V6 Loopback is \n {:#?}",
+        home, loopback
+    );
 }
